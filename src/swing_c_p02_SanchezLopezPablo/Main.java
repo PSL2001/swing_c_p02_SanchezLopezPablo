e53aa5ca43fb1c632795f222b371cb697c7a8fc2
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-11-17 13:50:25 +0100

    Initial Commit

---
+++ src/swing_c_p02_SanchezLopezPablo/Main.java
@@ -0,0 +1,22 @@
+/**
+ * swing_c_p02_SanchezLopezPablo - swing_c_p02_SanchezLopezPablo - Main.java
+ * 17 nov 2022 - 13:44:57
+ * @author Pablo Sánchez López
+ */
+package swing_c_p02_SanchezLopezPablo;
+
+/**
+ * @author usuario
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
